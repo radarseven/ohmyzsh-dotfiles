1b4b123a6d81a989e5fd83e55a6c5de66b669fce
Brandon Haslip <bhaslip@gmail.com>
2014-08-04 14:27:30 -0400

    Add makeItCraft alias

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 cd "$(dirname "${BASH_SOURCE}")"
-
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-GPL.txt" \
@@ -11,13 +10,11 @@ function doIt() {
 if [[ "$1" == "--force" || "$1" == "-f" ]]; then
 	doIt
 else
-	read -q "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) "
-	echo
+	echo -n  "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+	read REPLY
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
-
 unset doIt
-
 source ~/.zshrc
